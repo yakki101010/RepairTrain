98d430c74aea6a9fbbbd360353f81948201d6e4b
yakki <yukkiy.rakkiy.hapy928@gmail.com>
2026-01-01 19:59:23 +0900

    操作方法仮追加

---
+++ Assets/yakkiy/Script/HowToPlay.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class HowToPlay : MonoBehaviour
+{
+    public void GoHowToPlay()
+    {
+        Application.OpenURL("https://www.canva.com/design/DAG9DsjUIDE/1tu8k0c768WHb1SVUs6siA/edit?utm_content=DAG9DsjUIDE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton");
+    }
+}
